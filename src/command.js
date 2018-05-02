b0800bd477249cf5fea8cee2617e2e47282d9e36
Clarice Bouwer <clarice.bouwer@dstvdm.com>
2018-05-02 06:52:28 +0200

    Add execute movement and parse list of commands

---
+++ src/command.js
@@ -0,0 +1,36 @@
+import * as _coords from "./coords";
+import * as _cardinal from "./cardinal";
+
+/**
+ * Move the rover to its next co-ordinate and face in a specific direction based on the command.
+ * @param {Object[]} coord - The current co-ordinate the rover is stationed on.
+ * @param {string} cardinal - The current cardinal the rover is facing.
+ * @param {string} command - The command to be applied to the rover in order to move it.
+ * @return {string} A string in the format {x}{y} {cardinal} to be supplied to HQ.
+ */
+export function execute(coord, cardinal, command) {
+    if (!coord) throw new Error("`coord` parameter is required.");
+    if (!cardinal) throw new Error("`cardinal` parameter is required.");
+    if (!command) throw new Error("`command` parameter is required.");
+
+    cardinal = _cardinal.next(cardinal, command);
+    if (command === "M") {
+        coord = _coords.next(cardinal, coord);
+    }
+
+    var next = `${coord[0]}${coord[1]} ${cardinal}`;
+    return next;
+}
+
+/**
+ * Parse a single list of commands into an array.
+ * Valid commands include R(ight), L(eft) and M(ove).
+ * @param {string} commands - Single string containing commands for movement. No separators are expected. Example: LMRMM.
+ * @return {Object[]} An array of commands to be iterated for the calculations of movement.
+ */
+export function parseList(commands) {
+    if (!commands) throw new Error("`commands` parameter is required.");
+
+    var list = commands.toUpperCase().split("");
+    return list;
+}
---
+++ tests/command.test.js
@@ -0,0 +1,67 @@
+import { execute, parseList } from "../src/command";
+
+test("rover moves to the east", () => {
+    var actual = execute([1, 2], "E", "M");
+    expect(actual).toBe("22 E");
+});
+
+test("rover moves to the west", () => {
+    var actual = execute([1, 2], "W", "M");
+    expect(actual).toBe("02 W");
+});
+
+test("rover moves to the north", () => {
+    var actual = execute([1, 2], "N", "M");
+    expect(actual).toBe("11 N");
+});
+
+test("rover moves to the south", () => {
+    var actual = execute([1, 2], "S", "M");
+    expect(actual).toBe("13 S");
+});
+
+test("rover turns left from south", () => {
+    var actual = execute([1, 2], "S", "L");
+    expect(actual).toBe("12 E");
+});
+
+test("rover turns left from north", () => {
+    var actual = execute([1, 2], "N", "L");
+    expect(actual).toBe("12 W");
+});
+
+test("rover turns right from east", () => {
+    var actual = execute([1, 2], "E", "R");
+    expect(actual).toBe("12 S");
+});
+
+test("disallow execute: no parameter for coord", () => {
+    expect(() => { execute(null, "N", "M") }).toThrow("`coord` parameter is required.");
+});
+
+test("disallow execute: no parameter for cardinal", () => {
+    expect(() => { execute([0, 0], null, "L") }).toThrow("`cardinal` parameter is required.");
+});
+
+test("disallow execute: no parameter for command", () => {
+    expect(() => { execute([0, 0], "N", null) }).toThrow("`command` parameter is required.");
+});
+
+test("parse list of commands", () => {
+    var actual = parseList("LMRMM");
+    expect(actual).toEqual(["L", "M", "R", "M", "M"]);
+});
+
+test("parse list of commands does not validate", () => {
+    var actual = parseList("XYZ");
+    expect(actual).toEqual(["X", "Y", "Z"]);
+});
+
+test("parse list of commands converts commands to uppercase", () => {
+    var actual = parseList("xyz");
+    expect(actual).toEqual(["X", "Y", "Z"]);
+});
+
+test("disallow parseList: no parameter for commands", () => {
+    expect(() => { parseList() }).toThrow("`commands` parameter is required.");
+});
